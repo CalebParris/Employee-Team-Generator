5bd5ea92abea4b745442faaf3ef922d623979211
Caleb Parris <calebparris@live.com>
2020-09-17 13:17:50 -0500

    Added Prompts for team members

--- app.js
+++ app.js
@@ -33,3 +33,113 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+const employees = [];
+
+function managerInfo(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the Manager's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Manager's email?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the Manager's office number?"
+        }
+    ])
+    .then(function(answers){
+        console.log(answers);
+        
+        let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        employees.push(manager);
+        console.log(employees);
+
+        addTeam();
+    })
+}
+
+function addTeam(){
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "addMember",
+            message: "Do you want additional members on this team?"
+        }
+    ])
+    .then(function(answers){
+        console.log(answers);
+        if (answers.addMember){
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "nextRole",
+                    choices: ["Engineer", "Intern"],
+                    message: "Please choose from the following:"
+                }
+            ])
+            .then(function(answers){
+                console.log(answers);
+                let newRole, extraRoleInfo;
+                if (answers.nextRole === "Engineer"){
+                    newRole = "Engineer";
+                    extraRoleInfo = "Github username";
+                } else {
+                    newRole = "Intern";
+                    extraRoleInfo = "school name";
+                }
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        message: `What is the ${newRole}'s name?`
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        message: `What is the ${newRole}'s ID?`
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        message: `What is the ${newRole}'s email?`
+                    },
+                    {
+                        type: "input",
+                        name: "extraInfo",
+                        message: `What is the ${newRole}'s ${extraRoleInfo}?`
+                    }
+                ])
+                .then(function(answers){
+                    console.log(newRole);
+                    console.log(answers);
+                    if (newRole === "Engineer"){
+                        let engineer = new Engineer(answers.name, answers.id, answers.email, answers.extraInfo);
+                        employees.push(engineer);
+                        console.log(employees);
+                    } else {
+                        let intern = new Intern(answers.name, answers.id, answers.email, answers.extraInfo);
+                        employees.push(intern);
+                        console.log(employees);
+                    }
+
+                    addTeam();
+                })
+            })
+        } else {
+            return false;
+        }
+    })
+}
+
+managerInfo();
